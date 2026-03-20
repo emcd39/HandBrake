3a7b89afda35f99173e890210d6453e2312b04c6
emcd39 <65075014+emcd39@users.noreply.github.com>
2026-03-20 11:28:17 +0800

    Add jlesage-style RK3588 Docker build

--- docker/start.sh
+++ docker/start.sh
@@ -1,24 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-export DISPLAY=:0
-export HOME=/home/handbrake
-export LD_LIBRARY_PATH=/usr/lib/jellyfin/lib:$LD_LIBRARY_PATH
-export PATH=/usr/lib/jellyfin/bin:$PATH
+export HOME=/config
+export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
 
-wait_for_x() {
-    for i in {1..30}; do
-        if xdpyinfo -display :0 >/dev/null 2>&1; then
-            return 0
-        fi
-        sleep 1
-    done
-    return 1
-}
-
-if ! wait_for_x; then
-    echo "Error: X server not available"
-    exit 1
-fi
-
-exec /opt/handbrake/gtk/src/ghb
+exec /usr/local/bin/ghb
